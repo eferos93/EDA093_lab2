f25406a592f9eb42daa06753909ff35426f829c3
EmelieEken <emeeke@student.chalmers.se>
2017-10-09 15:25:00 +0200

    Updated to work on my Mac

--- thread.c
+++ thread.c
@@ -75,13 +75,10 @@ static tid_t allocate_tid (void);
    that's currently running into a thread.  This can't work in
    general and it is possible in this case only because loader.S
    was careful to put the bottom of the stack at a page boundary.
-
    Also initializes the run queue and the tid lock.
-
    After calling this function, be sure to initialize the page
    allocator before trying to create any threads with
    thread_create().
-
    It is not safe to call thread_current() until this function
    finishes. */
 void
@@ -151,14 +148,12 @@ thread_print_stats (void)
    PRIORITY, which executes FUNCTION passing AUX as the argument,
    and adds it to the ready queue.  Returns the thread identifier
    for the new thread, or TID_ERROR if creation fails.
-
    If thread_start() has been called, then the new thread may be
    scheduled before thread_create() returns.  It could even exit
    before thread_create() returns.  Contrariwise, the original
    thread may run for any amount of time before the new thread is
    scheduled.  Use a semaphore or some other form of
    synchronization if you need to ensure ordering.
-
    The code provided sets the new thread's `priority' member to
    PRIORITY, but no actual priority scheduling is implemented.
    Priority scheduling is the goal of Problem 1-3. */
@@ -214,7 +209,6 @@ thread_create (const char *name, int priority,
 
 /* Puts the current thread to sleep.  It will not be scheduled
    again until awoken by thread_unblock().
-
    This function must be called with interrupts turned off.  It
    is usually a better idea to use one of the synchronization
    primitives in synch.h. */
@@ -231,7 +225,6 @@ thread_block (void)
 /* Transitions a blocked thread T to the ready-to-run state.
    This is an error if T is not blocked.  (Use thread_yield() to
    make the running thread ready.)
-
    This function does not preempt the running thread.  This can
    be important: if the caller had disabled interrupts itself,
    it may expect that it can atomically unblock a thread and
@@ -243,15 +236,11 @@ thread_unblock (struct thread *t)
 
     ASSERT (is_thread (t));
 
-    //new
-    //if (thread_decrease_sleep() == 0) {
-
         old_level = intr_disable ();
         ASSERT (t->status == THREAD_BLOCKED);
         list_push_back (&ready_list, &t->elem);
         t->status = THREAD_READY;
         intr_set_level (old_level);
-    //}
 }
 
 /* Returns the name of the running thread. */
@@ -380,24 +369,29 @@ thread_get_load_avg (void)
   return 0;
 }
 
-int thread_decrease_sleep (struct thread *t, void *aux) { //CHANGED
+void thread_decrease_sleep (struct thread *t, void *aux) { //CHANGED
   if (t->status == THREAD_BLOCKED && t->ticks_to_sleep > 0) { //If sleeping
-    printf("TTS: %d\n",t->ticks_to_sleep);
+    //printf("TTS: %d\n",t->ticks_to_sleep);
     ASSERT (t->ticks_to_sleep > 0);
     t->ticks_to_sleep--;
     if (t->ticks_to_sleep == 0) { //If ready to be waken up and put on ready queue
+        enum intr_level old_level = intr_disable ();
         thread_unblock(t);
-        thread_yield();
+        //thread_yield();
+        intr_set_level (old_level);
       }
-    return t->ticks_to_sleep;
+    //return t->ticks_to_sleep;
   }
 }
 void thread_set_sleep (int time_sleep) { //CHANGED
     //intr_disable (); //Needed?
     //ASSERT not idle?
-    ASSERT (time_sleep > 0);
+    if (time_sleep > 0) { //If to be set asleep
+    //ASSERT (time_sleep > 0);
+    enum intr_level old_level = intr_disable ();
     thread_current()->ticks_to_sleep = time_sleep;
     thread_block ();
+    intr_set_level (old_level);
 }
 
 
@@ -410,7 +404,6 @@ thread_get_recent_cpu (void)
 }
 
 /* Idle thread.  Executes when no other thread is ready to run.
-
    The idle thread is initially put on the ready list by
    thread_start().  It will be scheduled once initially, at which
    point it initializes idle_thread, "up"s the semaphore passed
@@ -432,7 +425,6 @@ idle (void *idle_started_ UNUSED)
       thread_block ();
 
       /* Re-enable interrupts and wait for the next one.
-
          The `sti' instruction disables interrupts until the
          completion of the next instruction, so these two
          instructions are executed atomically.  This atomicity is
@@ -440,7 +432,6 @@ idle (void *idle_started_ UNUSED)
          between re-enabling interrupts and waiting for the next
          one to occur, wasting as much as one clock tick worth of
          time.
-
          See [IA32-v2a] "HLT", [IA32-v2b] "STI", and [IA32-v3a]
          7.11.1 "HLT Instruction". */
       asm volatile ("sti; hlt" : : : "memory");
@@ -526,18 +517,15 @@ next_thread_to_run (void)
 
 /* Completes a thread switch by activating the new thread's page
    tables, and, if the previous thread is dying, destroying it.
-
    At this function's invocation, we just switched from thread
    PREV, the new thread is already running, and interrupts are
    still disabled.  This function is normally invoked by
    thread_schedule() as its final action before returning, but
    the first time a thread is scheduled it is called by
    switch_entry() (see switch.S).
-
    It's not safe to call printf() until the thread switch is
    complete.  In practice that means that printf()s should be
    added at the end of the function.
-
    After this function and its caller returns, the thread switch
    is complete. */
 void
@@ -574,7 +562,6 @@ thread_schedule_tail (struct thread *prev)
    the running process's state must have been changed from
    running to some other state.  This function finds another
    thread to run and switches to it.
-
    It's not safe to call printf() until thread_schedule_tail()
    has completed. */
 static void

63314567190f1e53fb4a04ffe91c0dd831b0006d
eferos93 <eros.fabrici@gmail.com>
2017-10-10 15:45:36 +0200

    batch-scheduler changed

--- batch-scheduler.c
+++ batch-scheduler.c
@@ -6,7 +6,6 @@
 #include "threads/malloc.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
-#include "devices/timer.h"
 #include "lib/random.h" //generate random numbers
 
 #define BUS_CAPACITY 3
@@ -24,9 +23,11 @@ typedef struct {
 	int priority;
 } task_t;
 
-struct semaphore slotsFree;
-struct semaphore mutex;
-int current_direction;
+//struct semaphore slotsFree;
+struct condition waitingToGo[2][2];
+struct lock block;
+int currentDirection; //either 0 or 1
+int slotsFree;
 
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
         unsigned int num_priority_send, unsigned int num_priority_receive);
@@ -44,16 +45,16 @@ void oneTask(task_t task);/*Task requires to use the bus and executes methods be
 
 
 
-/* initializes semaphores */ 
-void init_bus(void){ 
- 
-    random_init((unsigned int)123456789); 
-    
-    //msg("NOT IMPLEMENTED");
-    sema_init(&slotsFree, BUS_CAPACITY); //CHANGED
-    current_direction = 0;
-    /* FIXME implement */
+/* initializes semaphores */
+void init_bus(void){
 
+    random_init((unsigned int)123456789);
+    slotsFree=BUS_CAPACITY;
+		for (int i=0; i<2; i++)
+			for (int j=0; j<2; j++)
+				cond_init(&waitingToGo[i][j]);
+		lock_init(&block);
+		currentDirection=SENDER;
 }
 
 /*
@@ -61,7 +62,7 @@ void init_bus(void){
  *  sending data to the accelerator and num_task_receive + num_priority_receive tasks
  *  reading data/results from the accelerator.
  *
- *  Every task is represented by its own thread. 
+ *  Every task is represented by its own thread.
  *  Task requires and gets slot on bus system (1)
  *  process data and the bus (2)
  *  Leave the bus (3).
@@ -70,8 +71,7 @@ void init_bus(void){
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
         unsigned int num_priority_send, unsigned int num_priority_receive)
 {
-    msg("NOT IMPLEMENTED");
-    /* FIXME implement */ //CHANGED
+	
 }
 
 /* Normal task,  sending data to the accelerator */
@@ -107,32 +107,40 @@ void oneTask(task_t task) {
 
 
 /* task tries to get slot on the bus subsystem */
-void getSlot(task_t task) 
+void getSlot(task_t task)
 {
-
-    //msg("NOT IMPLEMENTED");
-    /* FIXME implement */
-    //sema_ //Check if right direction first?
-    if (task.priority == 1) {
-            //Do smth
-    }
-    sema_down(&slotsFree); //CHANGED
+		lock_acquire(&block);
+		while(slotsFree==0 || slotsFree>0 && currentDirection!= task.direction) {
+			cond_wait(&waitingToGo[task.direction][task.priority], &block);
+		}
+		while(task.priority==NORMAL && !list_empty(&waitingToGo[task.direction][HIGH].waiters)){
+			cond_wait(&waitingToGo[task.direction][task.priority], &block);
+		}
+    slotsFree--;
+		currentDirection=task.direction;
+		lock_release(&block);
 }
 
 /* task processes data on the bus send/receive */
-void transferData(task_t task) 
+void transferData(task_t task)
 {
-    //msg("NOT IMPLEMENTED");
-    printf("Enters bus\n"); //CHANGED
-    timer_sleep(random_ulong() % 50); //Sleep for 0 to 49 ticks
-    printf("Exits bus\n");
-    /* FIXME implement */
+    printf("Enters bus\n");
+		timer_sleep(random_ulog() % 50);
+		printf("Exits bus\n", );
 }
 
 /* task releases the slot */
-void leaveSlot(task_t task)
+void leaveSlot(task_t task)  //CHANGED
 {
-    //msg("NOT IMPLEMENTED");
-    /* FIXME implement */
-    sema_up(&slotsFree);//CHANGED
+    lock_acquire(&block);
+		slotsFree++;
+		if(!list_empty(&(waitingToGo[currentDirection][HIGH].waiters))) {
+			cond_signal(&waitingToGo[currentDirection][HIGH], &block);
+		} else if(!list_empty(&waitingToGo[currentDirection][NORMAL].waiters)) {
+			cond_signal(&waitingToGo[currentDirection][NORMAL], &block);
+		} else if (slotsFree==BUS_CAPACITY) {
+			cond_broadcast(&waitingToGo[1-currentDirection][HIGH], &block);
+			cond_broadcast(&waitingToGo[1-currentDirection][NORMAL], &block);
+		}
+		lock_release(&block);
 }
